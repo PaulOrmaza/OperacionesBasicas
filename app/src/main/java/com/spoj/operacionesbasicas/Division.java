d20e591998bd31955af1859ec44d8933849f93a7
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-11-19 20:51:54 -0500

    Creacion de menu Division

--- app/src/main/java/com/spoj/operacionesbasicas/Division.java
+++ app/src/main/java/com/spoj/operacionesbasicas/Division.java
@@ -3,12 +3,19 @@ package com.spoj.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Division extends AppCompatActivity {
+    EditText edNum1,edNum2;
+    TextView tvTResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_division);
+        edNum1=findViewById(R.id.txtnum8);
+        edNum2=findViewById(R.id.txtnum9);
+        tvTResult=findViewById(R.id.txtresultado5);
     }
 }

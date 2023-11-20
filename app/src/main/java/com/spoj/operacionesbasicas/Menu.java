9d3d7f3a659a48d570c68c68eeef345c1f07fce4
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-11-19 19:57:13 -0500

    Configuracion de botones del Menu

--- app/src/main/java/com/spoj/operacionesbasicas/Menu.java
+++ app/src/main/java/com/spoj/operacionesbasicas/Menu.java
@@ -2,7 +2,10 @@ package com.spoj.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button btnSuma = findViewById(R.id.btnsuma);
+        Button btnResta = findViewById(R.id.btnresta);
+        Button btnMultiplicacion = findViewById(R.id.btnmult);
+        Button btnDivision = findViewById(R.id.btndiv);
+
+        btnSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Menu.this, Suma.class);
+                startActivity(intent);
+            }
+        });
+
+        btnResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Menu.this, Resta.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMultiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Menu.this, Multiplicacion.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                Intent intent = new Intent(Menu.this, Division.class);
+                startActivity(intent);
+            }
+        });
     }
 }

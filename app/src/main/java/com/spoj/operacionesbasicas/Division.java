3719f7025596d0fa1f291cf218aed93796a734d3
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-11-19 20:59:05 -0500

    Configuracion de Menu Division Creando variables para su uso y su metodo

--- app/src/main/java/com/spoj/operacionesbasicas/Division.java
+++ app/src/main/java/com/spoj/operacionesbasicas/Division.java
@@ -3,6 +3,7 @@ package com.spoj.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,5 +18,16 @@ public class Division extends AppCompatActivity {
         edNum1=findViewById(R.id.txtnum8);
         edNum2=findViewById(R.id.txtnum9);
         tvTResult=findViewById(R.id.txtresultado5);
+    }
+    public void calcularDivision(View v){
+        double num1=Double.parseDouble(edNum1.getText().toString());
+        double num2=Double.parseDouble(edNum2.getText().toString());
+        if (num2 != 0) {
+            double resultado = num1 / num2;
+            tvTResult.setText(resultado + "");
+        } else {
+
+            tvTResult.setText("Error: División por cero");
+        }
     }
 }

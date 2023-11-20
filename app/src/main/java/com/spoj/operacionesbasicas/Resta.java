4c3b890356cd1e75b8b5f356eb2a386f45bedeb1
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-11-19 20:33:34 -0500

    Configuracion de Menu Resta Creando variables para su uso

--- app/src/main/java/com/spoj/operacionesbasicas/Resta.java
+++ app/src/main/java/com/spoj/operacionesbasicas/Resta.java
@@ -3,12 +3,26 @@ package com.spoj.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Resta extends AppCompatActivity {
+    EditText edNum1,edNum2;
+    TextView tvTResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resta);
+        edNum1=findViewById(R.id.txtnum4);
+        edNum2=findViewById(R.id.txtnum5);
+        tvTResult=findViewById(R.id.txtresultado3);
+    }
+    public void calcularResta(View v){
+        double num1=Double.parseDouble(edNum1.getText().toString());
+        double num2=Double.parseDouble(edNum2.getText().toString());
+        double resultado=num1+num2;
+        tvTResult.setText(resultado+"");
     }
 }
